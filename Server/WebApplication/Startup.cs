57aacacb173d7aada3867d6ce30556b0c7c5ec00
duyndh <hoangduypok98@gmail.com>
2020-05-19 00:10:51 +0700

    Add swagger

--- Server/WebApplication/Startup.cs
+++ Server/WebApplication/Startup.cs
@@ -18,6 +18,7 @@ using WebApplication.Services;
 using Microsoft.EntityFrameworkCore;
 using WebApplication.Data;
 using AutoMapper;
+using Microsoft.OpenApi.Models;
 
 namespace WebApplication
 {
@@ -83,11 +84,28 @@ namespace WebApplication
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
