af0bf50fdc4bbb88e1cbbcc6ef7fbd8b50ed08a1
duyndh <hoangduypok98@gmail.com>
2020-07-13 00:46:42 +0700

    Add demo signalr for client

---
+++ Server/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44344/CallHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.Reconnecting += error =>
+            {
+                System.Diagnostics.Debug.Assert(connection.State == HubConnectionState.Reconnecting);
+
+                // Notify users the connection was lost and the client is reconnecting.
+                // Start queuing or dropping messages.
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += connectionId =>
+            {
+                System.Diagnostics.Debug.Assert(connection.State == HubConnectionState.Connected);
+
+                // Notify users the connection was reestablished.
+                // Start dequeuing messages queued while reconnecting if any.
+
+                return Task.CompletedTask;
+            };
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            connection.On<string>("ReceiveMessage", (message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+
+                await connection.InvokeAsync("AddToGroup", groupTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessageToGroup",
+                    groupTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+    }
+}
